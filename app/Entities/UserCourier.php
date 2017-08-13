403c5a0219852cb28f1d02994aaaa73d6fbf970f
Muhammad Afief Farista <afiefsky@gmail.com>
2017-08-13 23:40:37 +0700

    Adding another description on one of method within usercourier entity

--- app/Entities/UserCourier.php
+++ app/Entities/UserCourier.php
@@ -6,8 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserCourier extends Model
 {
+    /**
+     * @var string
+     */
     protected $table = 'users_couriers';
 
+    /**
+     * @var array
+     */
     protected $fillable = ['user_id', 'is_active'];
 
     // Relationship to Order Entity = One to Many; 'One' customer has/can do 'Many' orders
@@ -16,11 +22,17 @@ class UserCourier extends Model
         return $this->hasMany(Order::class);
     }
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function user()
     {
         return $this->belongsTo(User::class);
     }
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function location()
     {
         return $this->hasMany(CourierLocation::class, 'courier_id');
