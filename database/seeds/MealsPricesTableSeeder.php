272ca164e0dc08d29360166e26ab919d2d2ae308
Muhammad Afief Farista <afiefsky@gmail.com>
2017-08-20 13:22:07 +0700

    Added new meal

--- database/seeds/MealsPricesTableSeeder.php
+++ database/seeds/MealsPricesTableSeeder.php
@@ -36,5 +36,13 @@ class MealsPricesTableSeeder extends Seeder
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
         ]);
+
+        DB::table('meals_prices')->insert([
+            'meal_id' => 3,
+            'price' => 11000,
+            'is_valid' => 1,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
     }
 }
