9f7d3e41562a9ab431248308586c6c9a8714eff7
Muhammad Afief Farista <afiefsky@gmail.com>
2017-08-21 10:03:04 +0700

    fix url

--- routes/api.php
+++ routes/api.php
@@ -77,6 +77,10 @@ $api->version('v1', function($api) {
             $api->get('{meal_id}', 'MealController@getOne');
             $api->get('vendor/id={vendor_id}',
                 'MealController@get');
+
+            $api->get('vendor/{vendor_id}',
+                'MealController@get');
+
             $api->get('/', 'MealController@getAll');
         });
 
